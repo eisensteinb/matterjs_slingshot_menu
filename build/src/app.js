94f4cb6513bb2141808491217a02913658bc5e80
eisensteinb <95254408+eisensteinb@users.noreply.github.com>
2021-12-18 01:09:49 +0330

    Update app.js

--- build/src/app.js
+++ build/src/app.js
@@ -1,9 +1,3 @@
 const Slingshot = require('./slingshot');
 
 Slingshot.slingshotMenu();
-
-// const menu1 = document.getElementById('menu-1');
-// console.log(menu1);
-
-const menu2 = document.getElementById('menu-2');
-console.log(menu2);
